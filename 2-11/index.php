fc67908d8010981e029d40bb1d924182059d03c2
kouhei-r-g <103445080+kouhei-r-g@users.noreply.github.com>
2023-01-14 18:04:41 +0900

    課題提出2-11

---
+++ 2-11/index.php
@@ -0,0 +1,19 @@
+<form action="result.php" method="post">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス：<input type="text" name="mail">
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+
+<p>IT用語</p><br>
+<br>
+<p>■仕様書・設計書</p><br>
+<p>「仕様書」は完成イメージを明確にした資料であるのに対し、「設計書」は完成するまでの制作工程を明確にした資料</p><br>
+<p>■Git</p><br>
+<p>プログラムのソースコードなどの変更履歴を記録・追跡するための分散型バージョン管理システム</p><br>
+<p>■マージツール</p><br>
+<p>FDLファイル又はDTDファイルの変更内容をMDLファイルにマージできるツール</p><br>
+
