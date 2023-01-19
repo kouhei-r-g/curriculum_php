2067ecb5751a0ba4520f1147b8269755fb31dfcb
kouhei-r-g <103445080+kouhei-r-g@users.noreply.github.com>
2023-01-19 22:00:59 +0900

    課題提出2-18修正

--- 2-18/answer.php
+++ 2-18/answer.php
@@ -1,33 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-<main>
-    <?php 
-$my_name = $_POST["my_name"];
+<?php 
+    $my_name = $_POST["my_name"];
 
-//[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
-$answer1 = $_POST['answer1'];
-$answer2 = $_POST['answer2'];
-$answer3 = $_POST['answer3'];
-$number = $_POST["number"];
-$language = $_POST["language"];
-$comand = $_POST["comand"];
-//選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
+    //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
+    $answer1 = $_POST['answer1'];
+    $answer2 = $_POST['answer2'];
+    $answer3 = $_POST['answer3'];
+    $number = $_POST["number"];
+    $language = $_POST["language"];
+    $comand = $_POST["comand"];
+    //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
 
-function result($correctanswer,$answer){
-    if ($correctanswer == $answer) {
-        print "正解！";
-    }else {
-        print "残念・・・";
+    function result($correctanswer,$answer){
+        if ($correctanswer == $answer) {
+            print "正解！";
+        }else {
+            print "残念・・・";
+        }
     }
-}
 ?>
 
 
@@ -40,13 +29,24 @@ function result($correctanswer,$answer){
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php 
- result($answer2,$language);
+    result($answer2,$language);
 ?>
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php 
- result($answer3,$comand);
+    result($answer3,$comand);
 ?>
-    </main>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
 </body>
 </html>
--- 2-18/question.php
+++ 2-18/question.php
@@ -1,3 +1,55 @@
+<?php
+    //POST送信で送られてきた名前を受け取って変数を作成
+    $my_name = $_POST["my_name"];
+    //①画像を参考に問題文の選択肢の配列を作成してください。
+    $numbers = [80,22,20,21];
+    $language = ["PHP","Python","JAVA","HTML"];
+    $comand = ["join","select","insert","update"];
+    //② ①で作成した、配列から正解の選択肢の変数を作成してください
+    $answer1 = $numbers[0];
+    $answer2 = $language[3];
+    $answer3 = $comand[1];
+?>
+
+
+<p class="name">お疲れ様です<?php echo $my_name; ?>さん</p>
+<!--フォームの作成 通信はPOST通信で-->
+<form action="answer.php" method ="post">
+    <h2>ネットワークのポート番号は何番？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <?php
+        foreach( $numbers as $value ){?>
+            <input type="radio" name="number" value="<?php echo $value; ?>">
+            <?php echo $value;
+    }?>
+ 
+    <h2>②Webページを作成するための言語は？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <?php 
+        foreach( $language as $value ){?>
+            <input type="radio" name="language" value="<?php echo $value; ?>">
+            <?php echo $value;
+    }?>
+
+    <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <?php 
+        foreach( $comand as $value ){?>
+            <input type="radio" name="comand" value="<?php echo $value; ?>" >
+            <?php echo $value;
+    }?>
+
+    <br>
+
+    <input type="submit" value="回答する" >
+    <!--問題の正解の変数と名前の変数を[answer.php]に送る--> 
+        <input type="hidden" name="answer1" value="<?php echo $answer1; ?>">
+        <input type="hidden" name="answer2" value="<?php echo $answer2; ?>">
+        <input type="hidden" name="answer3" value="<?php echo $answer3; ?>">
+        <input type="hidden" name="my_name" value="<?php echo $my_name; ?>">
+</form>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,51 +60,6 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <main>
-    <?php
-//POST送信で送られてきた名前を受け取って変数を作成
-$my_name = $_POST["my_name"];
-//①画像を参考に問題文の選択肢の配列を作成してください。
-$numbers = [80,22,20,21];
-$language = ["PHP","Python","JAVA","HTML"];
-$comand = ["join","select","insert","update"];
-//② ①で作成した、配列から正解の選択肢の変数を作成してください
-$answer1 = $numbers[0];
-$answer2 = $language[3];
-$answer3 = $comand[1];
-?>
-<p class="name">お疲れ様です<?php echo $my_name; ?>さん</p>
-<!--フォームの作成 通信はPOST通信で-->
-<form action="answer.php" method ="post">
-    <h2>ネットワークのポート番号は何番？</h2>
-    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-    <?php
-    foreach( $numbers as $value ){?>
-        <input type="radio" name="number" value="<?php echo $value; ?>">
-       <?php echo $value;
-    }?>
- 
-    <h2>②Webページを作成するための言語は？</h2>
-    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-    <?php 
-        foreach( $language as $value ){?>
-        <input type="radio" name="language" value="<?php echo $value; ?>">
-       <?php echo $value;
-    }?>
-    <h2>③MySQLで情報を取得するためのコマンドは？</h2>
-    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-    <?php 
-        foreach( $comand as $value ){?>
-        <input type="radio" name="comand" value="<?php echo $value; ?>" >
-       <?php echo $value;
-    }?><br>
-    <input type="submit" value="回答する" >
-    <!--問題の正解の変数と名前の変数を[answer.php]に送る--> 
-        <input type="hidden" name="answer1" value="<?php echo $answer1; ?>">
-        <input type="hidden" name="answer2" value="<?php echo $answer2; ?>">
-        <input type="hidden" name="answer3" value="<?php echo $answer3; ?>">
-        <input type="hidden" name="my_name" value="<?php echo $my_name; ?>">
-</form>
-    </main>     
+    
 </body>
 </html>
